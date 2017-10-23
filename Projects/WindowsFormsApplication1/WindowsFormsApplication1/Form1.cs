273f4925a4432fd7dc42e2a4423bf43960ff6192
shiqin5898 <shiqin5898@163.com>
2017-10-23 17:58:06 +0800

    11.23

---
+++ Projects/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Should_auto_curry_two_number_add_function()
+        {
+            //Arrange
+            var add=_curringReasoning.AddTwoNumber();
+            var addCurrying = add.Curry();
+
+            //Act
+            var result = addCurrying(1)(2);
+            
+            //Assert
+            result.Should().Be(3);
+        }
+    }
+}
---
+++ Projects/WindowsFormsApplication1/WindowsFormsApplication1/扩展.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public static class ExtensionClass
+    {
+        public static Func<T1, Func<T2, TResult>> Curry<T1, T2, TResult>(this Func<T1, T2, TResult> func)
+        {
+            return x => y => func(x, y);
+        }
+
+        public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func)
+        {
+            return x => y => z => func(x, y, z);
+        }
+    }
+}
