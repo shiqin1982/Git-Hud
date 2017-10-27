e7934cee5b5b265d4c21b9c3f6642bab86116530
shiqin1982 <shiqin5898@163.com>
2017-10-27 10:16:00 +0800

    11.27

--- Projects/表达式计算器/表达式计算器/Form1.cs
+++ Projects/表达式计算器/表达式计算器/Form1.cs
@@ -35,6 +35,66 @@ namespace 表达式计算器
             double temp = 0;
             int dicimal = -1;
             double tempdi = 1;
+            for(int m=0;m<length;m++)
+            {
+                if(Char.IsDigit(a[m])==true)
+                {
+                    if(dicimal == -1)
+                    {
+                        temp*=10;
+                        temp+=a[m]-'0';
+                    }
+                    else
+                    {
+                        dicimal++;
+                        for(int q=0;q<dicimal;q++)
+                            tempdi=tempdi*0.1;
+                        temp=temp=(a[m]-'0')*tempdi;
+                    }
+                }
+                else if(a[m]=='.')
+                {
+                    dicimal=0;
+                }
+                else
+                {
+                    dicimal=-1;
+                    tempdi=1;
+                    data[datacount]=temp;
+                    datacount++;
+                    temp=0;
+                    oper[opercount]=a[m];
+                    opercount++;
+                }
+            }
+            data[datacount]=temp;
+            datacount++;
+            int temp1=0;
+            int tempdata=0;
+            while(temp1<opercount)
+            {
+                double para1=data[tempdata];
+                tempdata++;
+                double para2=data[tempdata];
+                if(oper[temp1]=='+')
+                {
+                    data[tempdata]=para1+para2;
+                }
+                else if(oper[temp1]=='-')
+                {
+                    data[tempdata]=para1-para2;
+                }
+                else if(oper[temp1]=='*')
+                {
+                    data[tempdata]=para1*para2;
+                }
+                else if(oper[temp1]=='/')
+                {
+                    data[tempdata]=para1/para2;
+                }
+                temp1++;
+            }
+            return data[tempdata];
         }
     }
 }
