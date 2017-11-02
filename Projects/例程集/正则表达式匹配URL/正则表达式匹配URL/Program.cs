57788509bec4c11823bb6039cad836adc43a9ffa
shiqin1982 <shiqin5898@163.com>
2017-11-02 15:13:53 +0800

    11.2

---
+++ Projects/例程集/正则表达式匹配URL/正则表达式匹配URL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+
+namespace 正则表达式匹配URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Pattern = @"^(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&$%\$#\=~])*$";
+            Regex r = new Regex(Pattern);
+            string source = "http://www.jb51.net";
+            Match m = r.Match(source);
+            if(m.Success)
+            {
+                Console.WriteLine("URL验证成功！");
+            }
+            else
+            {
+                Console.WriteLine("URL验证失败！");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Projects/例程集/正则表达式实例/正则表达式实例/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace 正则表达式实例
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("China", RegexOptions.IgnoreCase);
+            //使用Match方法
+            string source = "China is my MotherLand,my MotherLand is China!";
+            Match m = regex.Match(source);
+            if (m.Success)
+            {
+                Console.WriteLine("找到第一个匹配");
+            }
+            Console.WriteLine(new string('-', 9));
+            //演示使用Matches方法进行匹配
+            MatchCollection matches = regex.Matches(source);
+            foreach (Match s in matches)
+            {
+                if (s.Success)
+                    Console.WriteLine("找到了一个匹配");
+            }
+            Console.ReadLine();
+        }
+    }
+}
